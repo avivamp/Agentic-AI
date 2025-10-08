18ccc09b9c1c517a935e8928c70a3ad1886f63e5
avivamp <65968616+avivamp@users.noreply.github.com>
2025-10-08 09:51:20 +0400

    Delete emiratesred-scraper/convert_jsonl_to_json.py

--- emiratesred-scraper/convert_jsonl_to_json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("emiratesred_products.jsonl", "r", encoding="utf-8") as infile:
-    data = [json.loads(line) for line in infile]
-
-with open("emiratesred_products.json", "w", encoding="utf-8") as outfile:
-    json.dump(data, outfile, ensure_ascii=False, indent=2)
-
-print("✅ emiratesred_products.json created successfully")
